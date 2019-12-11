ce97f9e280e4f374bc1c9b84154a00283319c8bd
daffaattariq <daffaattariq19@gmail.com>
2019-12-11 13:00:40 +0700

    commit menu

--- app/src/main/java/id/ac/polinema/culinaryfood/activities/MenuActivity.java
+++ app/src/main/java/id/ac/polinema/culinaryfood/activities/MenuActivity.java
@@ -27,22 +27,121 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         Bundle extras = getIntent().getExtras();
-        String code="" , code_daerah;
+        String code="" , code_daerah="";
 
         if(extras!=null)
         {
+            code_daerah = extras.getString("code_daerah");
             code = extras.getString("kode_resto");
         }
 
 
         //String code = args.putString("Judul", getIntent().getExtras().getString("Judul"));
 
-        if(code.equals("jejemuran"))
+        if(code_daerah.equals("jogja"))
         {
-            resto_jejemuran();
+           if(code.equals("jejamuran"))
+           {
+               resto_jejamuran();
+           }
+
+           else if(code.equals("pelem_golek"))
+           {
+               resto_pelem_golek();
+           }
+           else if(code.equals("ledok_garden"))
+           {
+                ledok_garden();
+           }
+
+        }
+        else if(code_daerah.equals(("malang")))
+        {
+            if(code.equals("solaria"))
+            {
+                resto_solaria();
+            }
+
+            else if(code.equals("bensu"))
+            {
+                resto_bensu();
+            }
+            else if(code.equals("nelongso"))
+            {
+                resto_nelongso();
+            }
+        }
+        else if(code_daerah.equals(("surabaya")))
+        {
+            if(code.equals("asian_king"))
+            {
+                resto_asian_king();
+            }
+
+            else if(code.equals("primarasa"))
+            {
+                resto_primarasa();
+            }
+            else if(code.equals("khayangan"))
+            {
+                resto_khayangan();
+            }
+        }
+        else if(code_daerah.equals(("blitar")))
+        {
+            if(code.equals("warung_mak_nyak"))
+            {
+                resto_warung_mak_nyak();
+            }
+
+            else if(code.equals("rm_joglo"))
+            {
+                resto_rm_joglo();
+            }
+            else if(code.equals("soto_kudus"))
+            {
+                resto_soto_kudus();
+            }
         }
 
         buildRecyclerView();
+    }
+
+    private void resto_soto_kudus() {
+    }
+
+    private void resto_rm_joglo() {
+    }
+
+    private void resto_warung_mak_nyak() {
+    }
+
+    //surabaya
+    private void resto_khayangan() {
+    }
+
+    private void resto_primarasa() {
+    }
+
+    private void resto_asian_king() {
+    }
+
+    //malang
+    private void resto_bensu() {
+    }
+
+    private void resto_nelongso() {
+    }
+
+    private void resto_solaria() {
+    }
+
+
+    //jogja
+    private void ledok_garden() {
+    }
+
+    private void resto_pelem_golek() {
     }
 
     private void buildRecyclerView() {
@@ -56,7 +155,7 @@ public class MenuActivity extends AppCompatActivity {
         recyclerView.setAdapter(newAdapters);
     }
 
-    private void resto_jejemuran() {
+    private void resto_jejamuran() {
         menuModels =  new ArrayList<>();
         menuModels.add(new MenuModel(R.drawable.jejamuran,"ayam goreng","Rp.50000"));
     }
