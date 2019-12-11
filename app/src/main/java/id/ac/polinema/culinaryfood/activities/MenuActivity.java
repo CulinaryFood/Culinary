4a1da7eb750704b8a972309e0aaac40f0065c1fa
Alfarizqi88 <alfarizqi92@gmail.com>
2019-12-11 16:54:06 +0700

    update Menuactivity

--- app/src/main/java/id/ac/polinema/culinaryfood/activities/MenuActivity.java
+++ app/src/main/java/id/ac/polinema/culinaryfood/activities/MenuActivity.java
@@ -9,10 +9,8 @@ import android.os.Bundle;
 import java.util.ArrayList;
 
 import id.ac.polinema.culinaryfood.R;
-import id.ac.polinema.culinaryfood.adapters.Adapters;
 import id.ac.polinema.culinaryfood.adapters.NewAdapters;
 import id.ac.polinema.culinaryfood.models.MenuModel;
-import id.ac.polinema.culinaryfood.models.RestoModel;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -106,18 +104,30 @@ public class MenuActivity extends AppCompatActivity {
 
         buildRecyclerView();
     }
-
+    //blitar
     private void resto_soto_kudus() {
+        menuModels =  new ArrayList<>();
+        menuModels.add(new MenuModel(R.drawable.soto1,"Menu Soto Kudus Blitar.","Rp."));
     }
 
     private void resto_rm_joglo() {
+        menuModels =  new ArrayList<>();
+
     }
 
     private void resto_warung_mak_nyak() {
+        menuModels =  new ArrayList<>();
+        menuModels.add(new MenuModel(R.drawable.maknyak1,"Capjay Blitar","Rp.25000"));
+        menuModels.add(new MenuModel(R.drawable.maknyak2,"Gurami Goreng","Rp.45000"));
+        menuModels.add(new MenuModel(R.drawable.maknyak3,"Es Buah","Rp.12000"));
     }
 
     //surabaya
     private void resto_khayangan() {
+        menuModels =  new ArrayList<>();
+        menuModels.add(new MenuModel(R.drawable.kayangansby1,"Oseng Gurame","Rp.75000"));
+        menuModels.add(new MenuModel(R.drawable.kayangansby2,"Gurame Kribo (Krispi Boow)","Rp.75000"));
+
     }
 
     private void resto_primarasa() {
@@ -128,6 +138,11 @@ public class MenuActivity extends AppCompatActivity {
 
     //malang
     private void resto_bensu() {
+        menuModels =  new ArrayList<>();
+        menuModels.add(new MenuModel(R.drawable.bensu1,"Paket Garpu","Rp.5000"));
+        menuModels.add(new MenuModel(R.drawable.bensu2,"Paket Geprek Keju","Rp.20000"));
+        menuModels.add(new MenuModel(R.drawable.bensu3,"Paket Geprek","Rp.15000"));
+        menuModels.add(new MenuModel(R.drawable.bensu4,"Paket Geprek Leleh","Rp.25000"));
     }
 
     private void resto_nelongso() {
@@ -138,10 +153,26 @@ public class MenuActivity extends AppCompatActivity {
 
 
     //jogja
+    private void resto_jejamuran() {
+        menuModels =  new ArrayList<>();
+        menuModels.add(new MenuModel(R.drawable.jejamuran1,"Sate Jamur","Rp.8000"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran2,"Tongseng Jamur","Rp.8000"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran3,"Pepes Jamur Merang","Rp.5000"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran4,"Rendang Jamur","Rp.8000"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran5,"Sop Jamur","Rp.8000"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran6,"Jeruk Panas","Rp.4500"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran7,"Carica Squash","Rp.8000"));
+        menuModels.add(new MenuModel(R.drawable.jejamuran8,"Teh Panas","Rp.2500"));
+    }
     private void ledok_garden() {
+        menuModels =  new ArrayList<>();
+        menuModels.add(new MenuModel(R.drawable.ledokgarden1,"Nasgor Special Lemah Ledok","Rp.35000"));
+
     }
 
     private void resto_pelem_golek() {
+        menuModels =  new ArrayList<>();
+
     }
 
     private void buildRecyclerView() {
@@ -155,8 +186,5 @@ public class MenuActivity extends AppCompatActivity {
         recyclerView.setAdapter(newAdapters);
     }
 
-    private void resto_jejamuran() {
-        menuModels =  new ArrayList<>();
-        menuModels.add(new MenuModel(R.drawable.jejamuran,"ayam goreng","Rp.50000"));
-    }
+
 }
